b945ab21a243261749d11b13f20651c2c2641008
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-07-21 15:41:24 +1200

    add tsconfig.json and delete app.js since the compiler output is now directed to a js folder

--- app/app.js
+++
@@ -1,7 +0,0 @@
-function startGame() {
-    //starting a new game
-    var messagesElement = document.getElementById('messages');
-    //this allows the message to appear on the screen
-    messagesElement.innerText = 'Welcome to MultiMath! Starting new game...';
-}
-document.getElementById('startGame').addEventListener('click', startGame);
