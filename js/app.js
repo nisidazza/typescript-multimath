985f36c2636d1e476495354b028e943e314b26a2
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-07-21 16:02:22 +1200

    rename initial tsconfig file to tsconfig.base and add tsconfig in app folder

--- js/app.js
+++ js/app.js
@@ -1,8 +1,6 @@
 "use strict";
 function startGame() {
-    //starting a new game
     var messagesElement = document.getElementById('messages');
-    //this allows the message to appear on the screen
     messagesElement.innerText = 'Welcome to MultiMath! Starting new game...';
 }
 document.getElementById('startGame').addEventListener('click', startGame);
