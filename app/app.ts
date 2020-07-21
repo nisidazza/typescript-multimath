aec8a685dcc89c191253d99ab38eb5e71f847034
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-07-21 15:50:12 +1200

    quick fix to "Object is possibly null" error...
    ...since I know that messagesElement and the startGame element exist in index.html, I use non null operator to let typescript know that it can ignore this

--- app/app.ts
+++ app/app.ts
@@ -2,7 +2,7 @@ function startGame() {
     //starting a new game
     var messagesElement = document.getElementById('messages');
     //this allows the message to appear on the screen
-    messagesElement.innerText = 'Welcome to MultiMath! Starting new game...';
+    messagesElement!.innerText = 'Welcome to MultiMath! Starting new game...';
 }
 
-document.getElementById('startGame').addEventListener('click', startGame);
+document.getElementById('startGame')!.addEventListener('click', startGame);
---
+++ js/app.js
@@ -0,0 +1,9 @@
+"use strict";
+function startGame() {
+    //starting a new game
+    var messagesElement = document.getElementById('messages');
+    //this allows the message to appear on the screen
+    messagesElement.innerText = 'Welcome to MultiMath! Starting new game...';
+}
+document.getElementById('startGame').addEventListener('click', startGame);
+//# sourceMappingURL=app.js.map
