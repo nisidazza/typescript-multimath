c8eafc6cdbe90085fd952af5099cf8c48f71918c
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-07-21 17:02:05 +1200

    add logPlayer function and see how type annotation works

--- app/app.ts
+++ app/app.ts
@@ -1,8 +1,17 @@
 function startGame() {
     //starting a new game
+
+    let playerName: string = 'Audrey';
+    logPlayer(playerName);
+
+    
     var messagesElement = document.getElementById('messages');
     //this allows the message to appear on the screen
     messagesElement!.innerText = 'Welcome to MultiMath! Starting new game...';
+}
+
+function logPlayer(name: string) {
+    console.log(`New game starting for player: ${name}`);
 }
 
 document.getElementById('startGame')!.addEventListener('click', startGame);
---
+++ js/app.js
@@ -0,0 +1,12 @@
+"use strict";
+function startGame() {
+    var playerName = 'Audrey';
+    logPlayer(playerName);
+    var messagesElement = document.getElementById('messages');
+    messagesElement.innerText = 'Welcome to MultiMath! Starting new game...';
+}
+function logPlayer(name) {
+    console.log("New game starting for player: " + name);
+}
+document.getElementById('startGame').addEventListener('click', startGame);
+//# sourceMappingURL=app.js.map
