8571cfb712c58f9cc9b7b6a4d8d0cafeb8646d6f
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-07-23 10:32:45 +1200

    refactor utility utility.ts in order to be able to export the functions...
    ..aslo add logger function

--- app/utility.ts
+++ app/utility.ts
@@ -1,9 +1,12 @@
-class Utility {
-    //passing the id of an input box and get back the value in the box
-    /* add static keyword as I want to be able to use the function throughout the class without 
-    having to create instances of the utility class */
-    static getInputValue(elementID: string): string {
-        const inputElement: HTMLInputElement = <HTMLInputElement>document.getElementById(elementID);
-        return inputElement.value;
-    }
+
+//passing the id of an input box and get back the value in the box
+function getInputValue(elementID: string): string {
+    const inputElement: HTMLInputElement = <HTMLInputElement>document.getElementById(elementID);
+    return inputElement.value;
 }
+
+function logger(message: string): void {
+    console.log(message);
+}
+
+export { getInputValue as getValue, logger };
