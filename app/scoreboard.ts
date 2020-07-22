11925d2d0cdf5aec35aa19112dd933669af9b318
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-07-22 17:00:22 +1200

    add an array of objects that implements the Result interface and a method to add new results in the array

--- app/scoreboard.ts
+++ app/scoreboard.ts
@@ -1,1 +1,13 @@
 /// <reference path="result.ts" />
+
+//display the scores on the screen
+class Scoreboard {
+
+    //array of objs that implements the Result interface
+    private results: Result[] = []
+
+    //adds new results
+    addResult(newResult: Result): void {
+        this.results.push(newResult);
+    }
+}
