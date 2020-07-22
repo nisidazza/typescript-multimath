ac141429a0a23d552f51fb80b9cd0d202f4337b4
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-07-23 11:42:52 +1200

    reenable some webpack features to do the bundling and deliver the final javascript file to the Browser

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,22 @@
 module.exports = {
-    entry: './js/app.js',
-    // devtool: 'inline-source-map',
-    // module: {
-    //   rules: [
-    //     {
-    //       test: /\.tsx?$/,
-    //       use: 'ts-loader',
-    //       exclude: /node-modules/
-    //     }
-    //   ]
-    // },
-    // /*which files to use for module resolution*/
-    // resolve: {
-    //   extensions: [ '.tsx', '.ts', '.js' ]
-    // },
-    // output: {
-    //   filename: 'bundle.js'
-    // },
+    entry: './app/app.ts',
+    devtool: 'inline-source-map',
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node-modules/
+        }
+      ]
+    },
+    /*which files to use for module resolution*/
+    resolve: {
+      extensions: [ '.tsx', '.ts', '.js' ]
+    },
+    output: {
+      filename: 'bundle.js'
+    },
     devServer: {
       inline: false  /*prevents the browser to automatically refreshing when new bundles are produced - only for the purpose of this project */
     }
