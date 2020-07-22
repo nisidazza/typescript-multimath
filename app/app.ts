14e72fe1269903789e8a4b3765cd3a4a975eb60c
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-07-23 10:45:10 +1200

    import modules in app.ts and change code accordingly

--- app/app.ts
+++ app/app.ts
@@ -1,5 +1,6 @@
-/// <reference path="player.ts" />
-/// <reference path="game.ts" />
+import { Player } from './player';
+import { Game } from './game';
+import * as Helpers from './utility';
 
 let newGame: Game;
 
@@ -9,12 +10,12 @@ document.getElementById('startGame')!.addEventListener('click', () => {
     const player: Player = new Player();
     //and set its name to the value provided by the user
     //using the static getInputValue function by calling it directly on the utility class without creating an instance of the class
-    player.name = Utility.getInputValue('playername');
+    player.name = Helpers.getValue('playername');
 
     //retrieving values from the input boxes
     //using the built-in Number constructor to convert the input strings to numbers
-    const problemCount: number = Number(Utility.getInputValue('problemCount'));
-    const factor: number = Number(Utility.getInputValue('factor'));
+    const problemCount: number = Number(Helpers.getValue('problemCount'));
+    const factor: number = Number(Helpers.getValue('factor'));
 
     //create a Game instance and assign it to the newGame variable by passing the values to the constructor
     newGame = new Game(player, problemCount, factor);
